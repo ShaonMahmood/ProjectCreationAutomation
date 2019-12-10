b61bf66639d1123a5c73b3081ba177d766565636
ShaonMahmood <mahmood.habib.cuet@gmail.com>
2019-12-11 01:56:06 +0600

    version1 submitted

--- core/common.py
+++ core/common.py
@@ -3,46 +3,51 @@ import os,time
 from github import Github
 
 def create_git_repo(projectName,username, password):
-    user = Github(username, password).get_user()
+    try:
+        user = Github(username, password).get_user()
+    except Exception as e:
+        print("Username and password wrong")
+        return False
     repo = user.create_repo(projectName)
-    print("Succesfully created repository {}".format(project_name))
+    print("Succesfully created repository {}".format(projectName))
+    return True
 
+def create_project(username, password, project_directory="MyProjects", project_name="NewProject", venv_name=""):
+    present_dirname, present_filename = os.path.split(os.path.abspath(__file__))
+    root = os.path.dirname(present_dirname)
+    gitignore_file = os.path.join(root, '.gitignore')
 
-present_dirname, present_filename = os.path.split(os.path.abspath(__file__))
-root = os.path.dirname(present_dirname)
-gitignore_file = os.path.join(root,'.gitignore')
+    print(f"root is {root}, gitignore file is {gitignore_file}\n")
 
-print(f"root is {root}, gitignore file is {gitignore_file}\n")
+    remote_url = f'https://github.com/ShaonMahmood/{project_name}.git'
+    home_dir = os.path.expanduser('~')
+    project_dir_path = os.path.join(home_dir, project_directory, project_name)
+    if not os.path.isdir(project_dir_path):
+        if create_git_repo(project_name, username, password):
+            pass
+        else:
+            return 0
+        os.makedirs(project_dir_path)
+        os.chdir(project_dir_path)
+        subprocess.run('git init', shell=True)
+        time.sleep(2)
+        subprocess.run('touch README.md', shell=True)
+        subprocess.run(f'python3 -m venv {venv_name}_venv', shell=True)
+        time.sleep(2)
+        copy_command = f'cp {gitignore_file} {project_dir_path}'
+        print(f'copy command: {copy_command}')
+        subprocess.run(copy_command, shell=True)
+        subprocess.run(f'git remote add origin {remote_url}', shell=True)
+        subprocess.run('git add .', shell=True)
+        subprocess.run('git commit -m "Initial commit"', shell=True)
+        subprocess.run('git push -u origin master', shell=True)
+        subprocess.run('code .', shell=True)
+        time.sleep(2)
+        subprocess.run('exit', shell=True)
+        return 1
 
-project_dir_name = 'MyProjects'
-project_name = 'NewProject'
-remote_url = f'https://github.com/ShaonMahmood/{project_name}.git'
-home_dir = os.path.expanduser('~')
-project_dir_path = os.path.join(home_dir,project_dir_name,project_name)
-username = "" #Insert your github username here
-password = "" #Insert your github password here
-if not os.path.isdir(project_dir_path):
-    create_git_repo(project_name,username,password)
-    os.makedirs(project_dir_path)
-    os.chdir(project_dir_path)
-    subprocess.run('git init',shell=True)
-    time.sleep(2)
-    subprocess.run('touch README.md', shell=True)
-    subprocess.run('python3 -m venv venv', shell=True)
-    time.sleep(2)
-    copy_command =f'cp {gitignore_file} {project_dir_path}'
-    print(f'copy command: {copy_command}')
-    subprocess.run(copy_command, shell=True)
-    subprocess.run(f'git remote add origin {remote_url}', shell=True)
-    subprocess.run('git add .', shell=True)
-    subprocess.run('git commit -m "Initial commit"', shell=True)
-    subprocess.run('git push -u origin master', shell=True)
-    subprocess.run('code .', shell=True)
-    time.sleep(2)
-    subprocess.run('exit', shell=True)
-
-else:
-    print(f'project with the same name was created before')
-
+    else:
+        print(f'project with the same name was created before')
+        return 2
 
 
--- project_automation.py
+++ project_automation.py
@@ -1,101 +1,57 @@
-# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+from automation_gui import Ui_MainWindow  # importing our generated file
+from core.common import create_project
 
-# Form implementation generated from reading ui file 'automation_gui.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
+def show_message(text):
+    alert = QtWidgets.QMessageBox()
+    alert.setText(text)
+    alert.exec_()
 
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.github_heading = QtWidgets.QLabel(self.centralwidget)
-        self.github_heading.setGeometry(QtCore.QRect(320, 80, 131, 31))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.github_heading.setFont(font)
-        self.github_heading.setObjectName("github_heading")
-        self.username_label = QtWidgets.QLabel(self.centralwidget)
-        self.username_label.setGeometry(QtCore.QRect(210, 140, 101, 41))
-        self.username_label.setObjectName("username_label")
-        self.password_label = QtWidgets.QLabel(self.centralwidget)
-        self.password_label.setGeometry(QtCore.QRect(210, 200, 91, 41))
-        self.password_label.setObjectName("password_label")
-        self.username_text = QtWidgets.QTextEdit(self.centralwidget)
-        self.username_text.setGeometry(QtCore.QRect(330, 140, 201, 41))
-        self.username_text.setObjectName("username_text")
-        self.password_text = QtWidgets.QTextEdit(self.centralwidget)
-        self.password_text.setGeometry(QtCore.QRect(330, 200, 201, 41))
-        self.password_text.setObjectName("password_text")
-        self.project_heading = QtWidgets.QLabel(self.centralwidget)
-        self.project_heading.setGeometry(QtCore.QRect(320, 270, 111, 31))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.project_heading.setFont(font)
-        self.project_heading.setObjectName("project_heading")
-        self.project_directory_label = QtWidgets.QLabel(self.centralwidget)
-        self.project_directory_label.setGeometry(QtCore.QRect(210, 340, 111, 20))
-        self.project_directory_label.setObjectName("project_directory_label")
-        self.project_directory_text = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.project_directory_text.setGeometry(QtCore.QRect(330, 330, 201, 41))
-        self.project_directory_text.setPlainText("")
-        self.project_directory_text.setObjectName("project_directory_text")
-        self.project_name_label = QtWidgets.QLabel(self.centralwidget)
-        self.project_name_label.setGeometry(QtCore.QRect(210, 400, 101, 20))
-        self.project_name_label.setObjectName("project_name_label")
-        self.project_name_text = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.project_name_text.setGeometry(QtCore.QRect(330, 390, 201, 41))
-        self.project_name_text.setObjectName("project_name_text")
-        self.create_button = QtWidgets.QPushButton(self.centralwidget)
-        self.create_button.setGeometry(QtCore.QRect(512, 510, 161, 51))
-        self.create_button.setObjectName("create_button")
-        self.app_heading = QtWidgets.QLabel(self.centralwidget)
-        self.app_heading.setGeometry(QtCore.QRect(180, 20, 461, 41))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(18)
-        self.app_heading.setFont(font)
-        self.app_heading.setObjectName("app_heading")
-        self.venv_label = QtWidgets.QLabel(self.centralwidget)
-        self.venv_label.setGeometry(QtCore.QRect(210, 460, 111, 16))
-        self.venv_label.setObjectName("venv_label")
-        self.venv_text = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.venv_text.setGeometry(QtCore.QRect(330, 450, 201, 41))
-        self.venv_text.setObjectName("venv_text")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.github_heading.setText(_translate("MainWindow", "Github Credentials"))
-        self.username_label.setText(_translate("MainWindow", "User Name"))
-        self.password_label.setText(_translate("MainWindow", "Password"))
-        self.project_heading.setText(_translate("MainWindow", "Project Settings"))
-        self.project_directory_label.setText(_translate("MainWindow", "Project Directory"))
-        self.project_name_label.setText(_translate("MainWindow", "Project Name"))
-        self.create_button.setText(_translate("MainWindow", "Create New Project"))
-        self.app_heading.setText(_translate("MainWindow", "A Simple Desktop App For Creating New Python Projects"))
-        self.venv_label.setText(_translate("MainWindow", "Virtual Env Name"))
+        self.ui.create_button.clicked.connect(self.on_button_clicked)
+        self.ui.create_button.setToolTip("Click Here to Create Project")
+        self.ui.password_text.setEchoMode(QtWidgets.QLineEdit.Password)
 
 
-if __name__ == "__main__":
+
+    def on_button_clicked(self):
+        buttonReply = QtWidgets.QMessageBox.question(self,
+                                                     'Confirmation',
+                                                     "Do you want to create a new project?",
+                                                     QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                                     QtWidgets.QMessageBox.No)
+        if buttonReply == QtWidgets.QMessageBox.Yes:
+            username = self.ui.username_text.text()
+            password = self.ui.password_text.text()
+            project_directory = self.ui.project_directory_text.text()
+            project_name = self.ui.project_name_text.text()
+            venv_name = self.ui.venv_text.text()
+            response = create_project(username,password,project_directory,project_name,venv_name)
+
+            if response == 1:
+                show_message("project created successfully")
+                self.ui.project_directory_text.setText("")
+                self.ui.project_name_text.setText("")
+                self.ui.venv_text.setText("")
+            elif response == 0:
+                show_message("wrong username and password")
+
+            else:
+                show_message("project with the same name is already created")
+                self.ui.project_name_text.setText("")
+        else:
+            print("Nah")
+
+
+if __name__ == '__main__':
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+    app = QtWidgets.QApplication([])
+    application = mywindow()
+    application.show()
+    sys.exit(app.exec())
+
